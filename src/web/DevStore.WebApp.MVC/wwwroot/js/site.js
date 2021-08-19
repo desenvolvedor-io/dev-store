5ec0347b68d603a09dd6527031700282fd97e92a
Bruno Brito <bhdebrito@gmail.com>
2021-08-19 03:33:32 -0300

    refactor: translating

--- src/web/DevStore.WebApp.MVC/wwwroot/js/site.js
+++ src/web/DevStore.WebApp.MVC/wwwroot/js/site.js
@@ -1,63 +1,90 @@
 ﻿function BuscaCep() {
     $(document).ready(function () {
 
-        function limpa_formulário_cep() {
-            // Limpa valores do formulário de cep.
-            $("#Endereco_Logradouro").val("");
-            $("#Endereco_Bairro").val("");
-            $("#Endereco_Cidade").val("");
-            $("#Endereco_Estado").val("");
+        function limpa_formulário_ZipCode() {
+            // Limpa valores do formulário de ZipCode.
+            $("#Address_StreetAddress").val("");
+            $("#Address_Neighborhood").val("");
+            $("#Address_City").val("");
+            $("#Address_State").val("");
         }
 
-        //Quando o campo cep perde o foco.
-        $("#Endereco_Cep").blur(function () {
+        //Quando o campo ZipCode perde o foco.
+        $("#Address_ZipCode").blur(function () {
 
-            //Nova variável "cep" somente com dígitos.
-            var cep = $(this).val().replace(/\D/g, '');
+            //Nova variável "ZipCode" somente com dígitos.
+            var ZipCode = $(this).val().replace(/\D/g, '');
 
-            //Verifica se campo cep possui valor informado.
-            if (cep != "") {
+            //Verifica se campo ZipCode possui valor informado.
+            if (ZipCode != "") {
 
-                //Expressão regular para validar o CEP.
-                var validacep = /^[0-9]{8}$/;
+                //Expressão regular para validar o ZipCode.
+                var validaZipCode = /^[0-9]{8}$/;
 
-                //Valida o formato do CEP.
-                if (validacep.test(cep)) {
+                //Valida o formato do ZipCode.
+                if (validaZipCode.test(ZipCode)) {
 
                     //Preenche os campos com "..." enquanto consulta webservice.
-                    $("#Endereco_Logradouro").val("...");
-                    $("#Endereco_Bairro").val("...");
-                    $("#Endereco_Cidade").val("...");
-                    $("#Endereco_Estado").val("...");
+                    $("#Address_StreetAddress").val("...");
+                    $("#Address_Neighborhood").val("...");
+                    $("#Address_City").val("...");
+                    $("#Address_State").val("...");
 
-                    //Consulta o webservice viacep.com.br/
-                    $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?",
+                    //Consulta o webservice viaZipCode.com.br/
+                    $.getJSON("https://viacep.com.br/ws/" + ZipCode + "/json/?callback=?",
                         function (dados) {
 
                             if (!("erro" in dados)) {
                                 //Atualiza os campos com os valores da consulta.
-                                $("#Endereco_Logradouro").val(dados.logradouro);
-                                $("#Endereco_Bairro").val(dados.bairro);
-                                $("#Endereco_Cidade").val(dados.localidade);
-                                $("#Endereco_Estado").val(dados.uf);
+                                $("#Address_StreetAddress").val(dados.logradouro);
+                                $("#Address_Neighborhood").val(dados.bairro);
+                                $("#Address_City").val(dados.localidade);
+                                $("#Address_State").val(dados.uf);
                             } //end if.
                             else {
-                                //CEP pesquisado não foi encontrado.
-                                limpa_formulário_cep();
-                                alert("CEP não encontrado.");
+                                //ZipCode pesquisado não foi encontrado.
+                                limpa_formulário_ZipCode();
+                                alert("Zip Code not found.");
                             }
                         });
                 } //end if.
                 else {
-                    //cep é inválido.
-                    limpa_formulário_cep();
-                    alert("Formato de CEP inválido.");
+                    //ZipCode é inválido.
+                    limpa_formulário_ZipCode();
+                    alert("Invalid Zip Code.");
                 }
             } //end if.
             else {
-                //cep sem valor, limpa formulário.
-                limpa_formulário_cep();
+                //ZipCode sem valor, limpa formulário.
+                limpa_formulário_ZipCode();
             }
         });
     });
+}
+
+
+function CartaoCredito() {
+
+
+    if ($("#card_number").length > 0) {
+        $("#card_number").validateCreditCard(function (e) {
+
+            $(this).removeClass();
+            $(this).addClass("form-control");
+
+            if (e.card_type !== null)
+                $(this).addClass(e.card_type.name);
+
+            if (e.valid)
+                $(this).addClass("valid");
+            else
+                $(this).removeClass("valid");
+
+            if (e.length_valid && !e.valid) {
+                $(this).addClass("invalid");
+            } else {
+                $(this).removeClass("invalid");
+            }
+        });
+    }
 }
