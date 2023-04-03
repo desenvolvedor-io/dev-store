34e2e87563a4f151d598141b9c1b0fe3b04d4fa9
veysel mutlu <veysel_mutlu42@hotmail.com>
2023-04-03 22:57:03 +0300

    refactor: Configuration classes are managed under one roof.

---
+++ src/building-blocks/DevStore.Core/Configurations/BaseConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using DevStore.Core.DomainObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevStore.Core.Configurations
+{
+    public class BaseConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : Entity
+    {
+        public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.ToTable(GetTableName());
+        }
+
+        private string GetTableName() => $"{typeof(TEntity).Name}s";
+    }
+}
